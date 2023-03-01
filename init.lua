dc66893f752ec03ee49143dff12db1f33971ad15
Liam Mordue <Haruha@users.noreply.github.com>
2023-03-01 17:37:54 +0000

    Added segmentation of messages content to allow for multiple colours in a single chat message

--- init.lua
+++ init.lua
@@ -30,15 +30,36 @@ surface.CreateFont("chatbox60", {
 
 function printf(...) print(string.format(...)) end
 
+function table.shallow_copy(t)
+  local t2 = {}
+  for k,v in pairs(t) do
+    t2[k] = v
+  end
+  return t2
+end
+
 include('./vgui/message.lua')
 include('./vgui/base.lua')
 
 local base = vgui.Create('chatBase')
 
 local messages = {
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis suscipit augue. Cras tristique nisl libero, a luctus magna tincidunt sed. Maecenas ut mollis justo, vitae consequat elit. Etiam tincidunt id nunc at ultrices.',
-  'Et odio pellentesque diam volutpat commodo sed egestas. Orci eu lobortis elementum nibh tellus molestie nunc non. Quis viverra nibh cras pulvinar mattis nunc sed blandit. Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique.',
-  'Nisi lacus sed viverra tellus in hac habitasse. Morbi tristique senectus et netus et malesuada fames ac turpis. Lacus sed viverra tellus in hac habitasse platea. Cursus metus aliquam eleifend mi in.',
+  {
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    Color(240, 124, 60),
+    'In quis suscipit augue. Cras tristique nisl libero, a luctus magna tincidunt sed. ',
+    Color(156, 125, 232),
+    'Maecenas ut mollis justo, vitae consequat elit. Etiam tincidunt id nunc at ultrices.'
+  },
+  { 
+    Color(122, 160, 240),
+    'Et odio pellentesque diam volutpat commodo sed egestas. Orci eu lobortis elementum nibh',
+    Color(102, 219, 134),
+    'tellus molestie nunc non. Quis viverra nibh cras pulvinar mattis nunc sed blandit.',
+    Color(240, 240, 240), 
+    'Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique.',
+  },
+  --[['Nisi lacus sed viverra tellus in hac habitasse. Morbi tristique senectus et netus et malesuada fames ac turpis. Lacus sed viverra tellus in hac habitasse platea. Cursus metus aliquam eleifend mi in.',
   'Fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec. Donec ultrices tincidunt arcu non sodales. A arcu cursus vitae congue mauris rhoncus aenean vel.',
   'Eget dolor morbi non arcu. Egestas fringilla phasellus faucibus scelerisque eleifend. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a. Nunc non blandit massa enim nec dui nunc mattis enim.',
   'Eu tincidunt tortor aliquam nulla. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque.',
@@ -47,8 +68,8 @@ local messages = {
   'Accumsan sit amet nulla facilisi morbi tempus iaculis. Eu lobortis elementum nibh tellus molestie nunc non. Ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel.',
   'Eget felis eget nunc lobortis mattis aliquam faucibus purus in. Volutpat sed cras ornare arcu dui vivamus arcu felis. Montes nascetur ridiculus mus mauris.',
   'Condimentum lacinia quis vel eros donec. Lacus sed viverra tellus in. Neque gravida in fermentum et sollicitudin ac orci phasellus egestas.',
-  'Aenean vel elit scelerisque mauris pellentesque pulvinar. Sed velit dignissim sodales ut.',
-  'Maecenas et euismod mauris. Fusce tristique, ipsum a cursus interdum, felis sapien elementum sapien'
+  'Aenean vel elit scelerisque mauris pellentesque pulvinar. Sed velit dignissim sodales ut.', 
+  'Maecenas et euismod mauris. Fusce tristique, ipsum a cursus interdum, felis sapien elementum sapien'--]]
 }
 
 local count = 1
--- vgui/message.lua
+++ vgui/message.lua
@@ -1,14 +1,14 @@
 local PANEL = {}
+local draw, surface, string, table = draw, surface, string, table
 
-AccessorFunc(PANEL, 'm_sMessage', 'Message', FORCE_STRING)
 AccessorFunc(PANEL, 'm_sFont', 'Font', FORCE_STRING)
 AccessorFunc(PANEL, 'm_nFontSize', 'FontSize', FORCE_NUMBER)
-AccessorFunc(PANEL, 'm_Lines', 'Lines')
+AccessorFunc(PANEL, 'm_Message', 'Message')
+AccessorFunc(PANEL, 'm_Segments', 'Segments')
 
 function PANEL:Init() 
-  
   self:Dock(TOP)
-  self:DockMargin(0, 0, 0, 0)
+  self:DockMargin(4, 4, 4, 0)
 	-- self:LerpPositions(1, true)
   self:SetFont('chatbox18')
   self:SetFontSize(draw.GetFontHeight(self:GetFont()))
@@ -29,40 +29,96 @@ end
 
 function PANEL:GenerateLines()
   surface.SetFont(self:GetFont())
-  
-  local message = self:GetMessage()
 
-  -- Get sizes of panel, text
+  -- Get width of panel
   local selfWidth = self:GetWide() - (4 * 2)
   originalSelfWidth = selfWidth  -- Padding
 
-  local textWidth = surface.GetTextSize(message)
+  -- Hold all segments
+  local formattedSegments = {}
 
-  -- If the text size is less than the panel size, return early without wrapping
-  if (textWidth <= selfWidth - self.Avatar:GetWide()) then
-    self:SetLines({ message })
-    return
+  -- Current segment has a width, height, color, text
+  local currentSegment = {
+    x = 4 + self.Avatar:GetWide(), -- Offset by internal padding and avatar width 
+    y = 4,
+    w = 0,
+    h = 20,
+    color = Color(255, 255, 255),
+    text = ''
+  }
+
+  local lineWidth = currentSegment.x
+  local spaceWidth = surface.GetTextSize(' ')
+
+  -- Begin looping through the message
+  for k, messagePart in pairs(self:GetMessage()) do
+    if (type(messagePart) == 'table') then
+      -- table.insert(formattedSegments, table.shallow_copy(currentSegment))
+      currentSegment.color = messagePart
+      currentSegment.text = ''
+      currentSegment.x = 4 + currentSegment.x + currentSegment.w - spaceWidth
+      currentSegment.w = 0
+    end
+
+    if (type(messagePart) == 'string') then
+      -- Split the message by spaces
+      local splitText = string.Split(messagePart, ' ')
+
+      -- Loop the words
+      for k, word in pairs(splitText) do
+        local wordWidth = surface.GetTextSize(word)
+        -- If the word fits in the line, add it to the segment
+        if (lineWidth + wordWidth <= selfWidth) then
+          currentSegment.text = currentSegment.text .. ' ' .. word
+          currentSegment.w = currentSegment.w + wordWidth + spaceWidth
+          lineWidth = lineWidth + wordWidth + spaceWidth
+        else -- The current segment exceeds the line length, create a new one
+          table.insert(formattedSegments, table.shallow_copy(currentSegment))
+
+          -- Reset the cursor w, move cursor y down by text height, add new word
+          currentSegment.x = 4
+          currentSegment.y = currentSegment.y + currentSegment.h
+          currentSegment.w = wordWidth
+          currentSegment.text = word
+          lineWidth = wordWidth
+        end
+
+        if (k == #splitText) then
+          table.insert(formattedSegments, table.shallow_copy(currentSegment))
+        end 
+      end
+    end
   end
 
-  -- Hold all length-aware lines
-  local formattedLines = {}
 
-  -- Split the message by spaces
-  local splitText = string.Split(message, ' ')
-  local currentLine = ''
-
-  for k, word in pairs(splitText) do
-    -- TODO: Watch for edge case of word being bigger than panel here
+    --[[    -- TODO: Watch for edge case of word being bigger than panel here
     local lineWidth = surface.GetTextSize(currentLine)
     local wordWidth = surface.GetTextSize(word)
 
-    if (#formattedLines == 0) then
+    -- Locate starting tag inside word
+    local tag = '<r>'
+
+    local startPos, endPos = string.find(word, tag)
+    if (startPos != nil) then
+      -- Case 1 - the tag is at the start
+      if (startPos == 1) then
+        currentColor = Color(255, 0, 0) -- Switch out current colour
+        word = string.gsub(word, tag, '') -- Remove tag from text
+        
+        -- Recalculate size of current word
+        wordWidth = surface.GetTextSize(word)
+      end
+      -- Case 2 - the tag is in the middle
+      -- Case 3 - the tag is at the end of the word
+    end
+
+    if (#formattedSegments == 0) then
       selfWidth = originalSelfWidth - self.Avatar:GetWide() -- Reduce original width copy
     end
     
     -- If the current line width is greater than the width of panel, save line and reset
     if ((lineWidth + wordWidth) >= selfWidth) then
-      table.insert(formattedLines, string.Trim(currentLine))
+      table.insert(formattedSegments, { text = string.Trim(currentLine), color = currentColor })
       currentLine = word
     else
       currentLine = currentLine .. ' ' .. word
@@ -70,17 +126,35 @@ function PANEL:GenerateLines()
 
     -- If this is the last word, add the line before reaching limit
     if (k == #splitText) then
-      table.insert(formattedLines, string.Trim(currentLine))
+      table.insert(formattedSegments, { text = string.Trim(currentLine), color = currentColor })
     end
 
     -- Reset width if we're past the first line
-    if (#formattedLines == 1) then
+    if (#formattedSegments == 1) then
       selfWidth = originalSelfWidth
     end
+   
 
+  end --]]
+
+  self:SetSegments(formattedSegments)
+end
+
+/*
+ * Gets the width of a given segment in px
+ */
+function PANEL:GetSegmentWidth(segment)
+  local width = 0
+
+  surface.SetFont(self:GetFont())
+
+  for k, v in pairs(segment) do
+    if (type(v) == 'string') then
+      width = width + surface.GetTextSize(v)
+    end
   end
 
-  self:SetLines(formattedLines)
+  return width
 end
 
 function PANEL:PerformLayout(width, height)
@@ -97,31 +171,48 @@ function PANEL:PerformLayout(width, height)
   self.TextStart.Paint = function() end
 
   self:GenerateLines()
-  self:SetHeight((#self:GetLines() * self:GetFontSize()) + 8)
+
+  local lastSegment = self:GetSegments()[#self:GetSegments()]
+  self:SetHeight(lastSegment.y + lastSegment.h)
+
+  -- PrintTable(self:GetSegments())
 end
 
 function PANEL:Paint(width, height)
-  if self:GetMessage() != nil then
-    -- draw.RoundedBox(4, 0, 0, width, height, Color(0, 0, 0))
+  if (self:GetMessage() != nil) then
 
+    draw.RoundedBox(4, 0, 0, width, height, Color(31, 31, 31))
+    --[[
     for k, v in pairs(self:GetLines()) do
       local widthOffset = 4
       if (k == 1) then
         widthOffset = widthOffset + self.Avatar:GetWide()
       end
-      draw.SimpleText(v, self:GetFont(), widthOffset, 4 + ((k - 1) * self:GetFontSize()), Color(240, 240, 240), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP) 
+      draw.SimpleText(v.text, self:GetFont(), widthOffset, 4 + ((k - 1) * self:GetFontSize()), v.color, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP) 
+    end
+    --]]
+
+    for k, v in pairs(self:GetSegments()) do
+      draw.SimpleText(v.text, self:GetFont(), v.x, v.y, v.color, TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
     end
   end
 end
 
 function PANEL:OnMouseReleased(control)
-	if control == MOUSE_RIGHT then
+	if (control == MOUSE_RIGHT) then
 
 		local menu = DermaMenu()
 
 		menu:AddOption('Copy', function() 
-      if IsValid(self) then
-        SetClipboardText(self:GetMessage())
+      if (ValidPanel(self)) then
+        local message = ''
+        for k, v in pairs(self:GetMessage()) do
+          if (type(v) == 'string') then
+            message = message .. v
+          end
+        end
+        
+        SetClipboardText(message)
       end
     end)
 
